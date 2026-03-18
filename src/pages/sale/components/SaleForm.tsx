ec31d3f21319116e6b3d74526a83729189f111b9
david3010M <hvaldiviezos@unprg.edu.pe>
2026-03-18 09:46:14 -0500

    Update SaleForm.tsx

--- src/pages/sale/components/SaleForm.tsx
+++ src/pages/sale/components/SaleForm.tsx
@@ -1836,7 +1836,7 @@ export const SaleForm = ({
                   name="temp_amount"
                   label="Monto"
                   type="number"
-                  step="0.0001"
+                  step="0.01"
                   min={0}
                   placeholder="0.00"
                 />
